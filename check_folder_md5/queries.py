df96696ddaa2ace2ad5ef7415652219d6f9304ae
Luis J. Villanueva <villanueval@si.edu>
2020-03-23 15:34:41 -0400

    Fixing queries

--- check_folder_md5/queries.py
+++ check_folder_md5/queries.py
@@ -10,8 +10,8 @@ folder_processing_update = "UPDATE folders SET processing_md5 = %(processing)s W
 
 select_folderid = "SELECT folder_id FROM folders WHERE project_folder = %(project_folder)s and path = %(folder_path)s and project_id = %(project_id)s"
 
-file_postprocessing1 = "INSERT INTO file_postprocessing (file_id, post_step, post_results) VALUES (SELECT file_id, 'md5_matches', 0 FROM files WHERE folder_id =  %(folder_id)s)"
+file_postprocessing1 = "INSERT INTO file_postprocessing (file_id, post_step, post_results) (SELECT file_id, 'md5_matches', 0 FROM files WHERE folder_id =  %(folder_id)s)"
 
-file_postprocessing2 = "INSERT INTO file_postprocessing (file_id, post_step, post_results) VALUES (SELECT file_id, 'ready_for_dams', 9 FROM files WHERE folder_id =  %(folder_id)s)"
+file_postprocessing2 = "INSERT INTO file_postprocessing (file_id, post_step, post_results) (SELECT file_id, 'ready_for_dams', 9 FROM files WHERE folder_id =  %(folder_id)s)"
 
-file_postprocessing3 = "INSERT INTO file_postprocessing (file_id, post_step, post_results) VALUES (SELECT file_id, 'in_dams', 9 FROM files WHERE folder_id =  %(folder_id)s)"
+file_postprocessing3 = "INSERT INTO file_postprocessing (file_id, post_step, post_results) (SELECT file_id, 'in_dams', 9 FROM files WHERE folder_id =  %(folder_id)s)"
