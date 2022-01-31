da8ff38f5fa593c8357d8b064bc77082b7e72239
Luis J. Villanueva <villanueval@si.edu>
2022-01-31 10:25:55 -0500

    Simplifications, ver 1.0

--- image_validation/settings.py.template.py
+++ image_validation/settings.py.template.py
@@ -1,88 +1,80 @@
-# ID of the project
+# ID of the project, must exist in the database already
 project_id = ""
 
-# List of paths where the folders to check are
+
+# List of paths where the folders are stored
 project_paths = []
 
+
 # List of shares used in the project,
-# each in its own list: ["mount point", "server"]
+# each in its own list: ["mount point", "server name"]
 project_shares = []
 
-# Osprey folder
-# If None, then it uses the cwd
-osprey_folder = None
 
-#Temp folder, usually /tmp
+# Logs folder
+# If None, then it uses a logs folder in the the cwd
+log_folder = None
+
+
+# Temp folder, usually /tmp
 # No trailing slash
 tmp_folder = "/tmp"
 
 
-####################################################################
+# Subfolders of files
+main_files_path = "tifs"
+raw_files_path = "raws"
+derivative_files_path = "jpgs"
+
+
 # Postgres database and rw user,
-####################################################################
 db_host = ""
 db_db = ""
 db_user = ""
 db_password = ""
-####################################################################
 
 
-####################################################################
-# What kind of product will be produced in the project
-####################################################################
-project_type = "tif"
-# Options are:
-# - tif
-# - jpg
-####################################################################
+# Parallel workers
+no_workers = 2
 
 
 ####################################################################
 # Project checks to run
 ####################################################################
-project_file_checks = ["raw_pair", "magick", "jhove", "unique_file", "tifpages"]
-# For TIFS:
-# - raw_pair: The raw file with the extension in 'raw_files' paired to the tif
-# - tif_size: Size of the tif file
-# - raw_size: Size of the raw file
-# - jpg: Check for a jpg file for each tif
-# - tifpages: Check the number of pages in the tif
-# - tif_compression: Check if tif is compressed using LZW
+project_file_checks = ["jhove", "magick", "raw_pair", "unique_file", "tifpages"]
+# The options are:
 #
-# For TIFS or JPG:
-# - magick: Run imagemagick validation
-# - stitched_jpg: There is a stitched JPG of two other files
-# - itpc: Check for valid ITPC metadata, not yet implemented
-#
-# For either:
-# - jhove: Run jhove validation
+# - raw_pair: Is there a raw file in the 'raw_files_path'
+# - valid_name: Filename is in the list of allowed names
 # - unique_file: Name is not repeated in the project
-# - valid_name: Check if the filename is in the list of allowed names
-# - prefix: Check if the filename has the required prefix
 # - old_name: Check name against the old_names table
+# - jhove: Run jhove validation
+# - magick: Run imagemagick validation
+# - tifpages: Check the number of pages in the tif, typically a thumbnail
+# - tif_compression: Check if tif is compressed using LZW
+# - derivative: Check for a derivative file in 'derivative_files_path'
+#
+# Specify these options below:
+# - prefix: Filename has the required prefix
+# - suffix: Filename has the required suffix
+# - stitched_jpg: There is a stitched JPG of two other files, settings below
 ####################################################################
 
-# Special (rare) checks
-special_checks = []
-
-
-folder_name = ""
 
 # How to split to parse the date, return the date in format 'YYYY-MM-DD'
 def folder_date(folder_name):
-    folder_date = folder_name.split('SG-CGA-Druse-')[1]
+    # Example as PREFIX-YYYYMMDD
+    folder_date = folder_name.split('PREFIX-')[1]
     formatted_date = "{}-{}-{}".format(folder_date[0:4], folder_date[4:6], folder_date[6:8])
     return formatted_date
 
 
-# Raw files extension
-raw_files = "iiq"
+# Filename prefix required by selecting 'prefix' above
+filename_prefix = None
 
 
-# Subfolder of files
-tif_files_path = "tifs"
-raw_files_path = "raws"
-jpg_files_path = "jpgs"
+# Filename suffix required by selecting 'suffix' above
+filename_suffix = None
 
 
 # Should the names match a db?
@@ -90,17 +82,7 @@ files_db = False
 # How to find the list of valid names
 # this has to be a valid query that will 
 # be used in a subquery
-filename_pattern_query = "SELECT COUNT(*) as no_records FROM (SELECT CONCAT('AC0433-', LPAD(file_name::text, 6, '0'), '%') AS file_name FROM valid_names WHERE project_id = 104) a WHERE a.file_name LIKE '{}%'"
-
-
-# TIF file min and max size, in bytes
-tif_size_min = 70000000
-tif_size_max = 390000000
-
-
-# RAW file min and max size, in bytes
-raw_size_min = 50000000
-raw_size_max = 300000000
+filename_pattern_query = "SELECT COUNT(*) as no_records FROM (SELECT file_name FROM filename_table WHERE project_id = 100 AND file_name = '{}') a"
 
 
 # How long to sleep between loops
@@ -109,32 +91,28 @@ raw_size_max = 300000000
 sleep = 180
 
 
-# Path for JHOVE
-jhove_path = "/home/[user]/jhove/jhove"
+# Path for JHOVE in system
+jhove_path = "/usr/local/jhove/jhove"
 
 
-# JPG Previews location and size
+# Path of where to save the JPG previews and size
 jpg_previews = ""
-# Set previews_size = 'full' to keep the same size as the TIF
-previews_size = 1000
+# Set previews_size = 'full' to keep the same size as the main image
+previews_size = "1000"
 
 
 # Ignore files with this string anywhere in the filename
 ignore_string = None
 
 
-del_folders = True
-
-
 # stitched_jpg
+# Check for stitched jpg files from two mains
 jpgstitch_original_1 = ""
 jpgstitch_original_2 = ""
 jpgstitch_new = ""
 
 
-# Check or ignore deleted files
-check_deleted = False
+# Scripts to run before or after checking the files. Set to None if it won't be used.
+pre_script = None
+post_script = None
 
-
-# prefix
-filename_prefix = ""
