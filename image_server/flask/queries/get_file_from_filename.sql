5a97f3bbd55d7c84d1e3270734ed2aeebd581df3
Luis J. Villanueva <villanueval@si.edu>
2023-06-08 13:46:15 -0400

    Update get_file_from_filename.sql

--- image_server/flask/queries/get_file_from_filename.sql
+++ image_server/flask/queries/get_file_from_filename.sql
@@ -1,1 +1,2 @@
-SELECT file_id, folder_id, preview_image FROM files WHERE CAST(file_name AS int) = CAST(%(file_name)s AS int) AND folder_id IN (SELECT folder_id FROM folders WHERE project_id = ANY('{100,131}')) LIMIT 1
+SELECT file_id, folder_id, preview_image FROM files WHERE file_name = %(file_name)s AND folder_id IN (SELECT folder_id FROM folders WHERE project_id in(100,131)) LIMIT 1
+
