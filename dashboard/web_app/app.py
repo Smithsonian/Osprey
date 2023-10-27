1560a6e09703d85de5924fc73fcc755639c2bd3f
Luis J. Villanueva <villanueval@si.edu>
2023-10-27 14:38:17 -0400

    Adding button to update DAMS status

--- dashboard/web_app/app.py
+++ dashboard/web_app/app.py
@@ -43,7 +43,7 @@ from PIL import Image
 import settings
 
 
-site_ver = "2.6.2"
+site_ver = "2.6.3"
 site_env = settings.env
 site_net = settings.site_net
 
@@ -2892,6 +2892,130 @@ def search_folders(project_alias):
                            user_address=user_address)
 
 
+@app.route('/folder_update/<project_alias>/<folder_id>', methods=['GET'], strict_slashes=False, provide_automatic_options=False)
+@login_required
+def update_folder_dams(project_alias=None, folder_id=None):
+
+    if folder_id is None or project_alias is None:
+        return redirect(url_for('home'))
+
+    """Update folder when sending to DAMS"""
+    # Connect to db
+    try:
+        conn = pymysql.connect(host=settings.host,
+                               user=settings.user,
+                               passwd=settings.password,
+                               database=settings.database,
+                               port=settings.port,
+                               charset='utf8mb4',
+                               cursorclass=pymysql.cursors.DictCursor,
+                               autocommit=True)
+        cur = conn.cursor()
+    except pymysql.Error as e:
+        logging.error(e)
+        raise InvalidUsage('System error')
+
+    # Del DAMS status badge, if exists
+    delbadge = query_database_insert(
+            ("DELETE FROM folders_badges WHERE folder_id = %(folder_id)s AND badge_type = 'dams_status'"),
+                {'folder_id': folder_id}, cur=cur)
+
+    # Set as Ready for DAMS
+    delbadge = query_database_insert(
+            ("INSERT INTO folders_badges (folder_id, badge_type, badge_css, badge_text) "
+             " VALUES (%(folder_id)s, 'dams_status', 'bg-secondary', 'Ready for DAMS')"),
+                {'folder_id': folder_id}, cur=cur)
+
+    # Update post-proc
+    delbadge = query_database_insert(
+            ("""
+                INSERT INTO file_postprocessing
+                    (file_id, post_results, post_step)
+                (
+                    SELECT
+                        file_id, 0 as post_results, 'ready_for_dams' as post_step
+                    FROM
+                         (SELECT file_id FROM files WHERE folder_id = %(folder_id)s)
+                        a
+                ) ON
+                DUPLICATE KEY UPDATE
+                post_results = 0
+            """),
+                {'folder_id': folder_id}, cur=cur)
+
+    # Update DAMS UAN
+    delbadge = query_database_insert(
+            ("""
+                UPDATE files f,
+                (
+                    SELECT f.file_id, d.dams_uan
+                    FROM
+                        dams_cdis_file_status_view_dpo d,
+                        files f,
+                        folders fold,
+                        projects p
+                    WHERE
+                        fold.folder_id = f.folder_id AND
+                        fold.project_id = p.project_id AND
+                        d.project_cd = p.dams_project_cd AND
+                        d.file_name = CONCAT(f.file_name, '.tif') AND
+                        f.folder_id =   %(folder_id)s
+                ) d
+                SET f.dams_uan = d.dams_uan
+                WHERE f.file_id = d.file_id
+            """),
+                {'folder_id': folder_id}, cur=cur)
+
+    # Update in DAMS
+    damsupdate = query_database_insert(
+            ("""
+                INSERT INTO file_postprocessing
+                    (file_id, post_results, post_step)
+                (
+                    SELECT
+                         file_id,
+                         0 as post_results,
+                         'in_dams' as post_step
+                    FROM
+                     (
+                     SELECT file_id FROM files
+                     WHERE
+                        folder_id = %(folder_id)s AND 
+                        dams_uan != '' AND dams_uan IS NOT NULL
+                     )
+                    a
+                ) ON
+                DUPLICATE KEY UPDATE post_results = 0
+            """),
+                {'folder_id': folder_id}, cur=cur)
+
+    no_files_ready = run_query(
+        ("SELECT COUNT(*) as no_files FROM files WHERE folder_id = %(folder_id)s AND dams_uan != '' AND dams_uan IS NOT NULL"),
+        {'folder_id': folder_id}, cur=cur)
+
+    no_files_pending = run_query(
+        ("SELECT COUNT(*) as no_files FROM files WHERE folder_id = %(folder_id)s AND (dams_uan = '' OR dams_uan IS NULL)"),
+        {'folder_id': folder_id}, cur=cur)
+
+    if no_files_ready[0]['no_files'] > 0 and no_files_pending[0]['no_files'] == 0:
+        # Update in DAMS
+        damsupdate = query_database_insert(
+            ("UPDATE folders SET delivered_to_dams = 0 WHERE folder_id = %(folder_id)s"),
+            {'folder_id': folder_id}, cur=cur)
+        damsupdate = query_database_insert(
+            ("DELETE FROM folders_badges WHERE folder_id = %(folder_id)s AND badge_type = 'dams_status'"),
+            {'folder_id': folder_id}, cur=cur)
+        damsupdate = query_database_insert(
+            ("""
+                INSERT INTO folders_badges 
+                    (folder_id, badge_type, badge_css, badge_text) VALUES 
+                    (%(folder_id)s, 'dams_status', 'bg-success', 'Delivered to DAMS')
+            """), {'folder_id': folder_id}, cur=cur)
+    cur.close()
+    conn.close()
+    return redirect(url_for('dashboard_f', project_alias=project_alias, folder_id=folder_id))
+
+
 @app.route("/logout", methods=['GET'], strict_slashes=False, provide_automatic_options=False)
 def logout():
     logout_user()
@@ -3811,6 +3935,9 @@ def data_reports_form():
 @app.route('/reports/<project_alias>/<report_id>/', methods=['GET'], strict_slashes=False, provide_automatic_options=False)
 def data_reports(project_alias=None, report_id=None):
     """Report of a project"""
+
+    # Declare the login form
+    form = LoginForm(request.form)
 
     # Connect to db
     try:
