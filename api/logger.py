793c50e126b0e0235469c586dc8bccc7d9103218
Luis J. Villanueva <villanueval@si.edu>
2024-11-19 13:46:44 -0500

    Adding deepzoom viewer

---
+++ api/logger.py
@@ -0,0 +1,36 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+from time import strftime
+from time import localtime
+import gzip
+import shutil
+import settings
+
+# Logging
+current_time = strftime("%Y%m%d_%H%M%S", localtime())
+
+# Create folder if it doesn't exists
+# os.makedirs('logs', exist_ok=True)
+
+
+# From https://docs.python.org/3/howto/logging-cookbook.html#using-a-rotator-and-namer-to-customize-log-rotation-processing
+def rotator(source, dest):
+    with open(source, 'rb') as f_in:
+        with gzip.open(dest, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    os.remove(source)
+
+
+def namer(name):
+    return name + ".gz"
+
+
+logfile = '{}/ospreyapp_{}.log'.format(settings.log_folder, current_time)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(levelname)s | %(asctime)s | %(filename)s:%(lineno)s | %(message)s',
+                    datefmt='%y-%b-%d %H:%M:%S',
+                    handlers=[RotatingFileHandler(logfile, maxBytes=10000000, backupCount=10)])
+logging.rotator = rotator
+logging.namer = namer
+logger = logging.getLogger("osprey_webapp")
